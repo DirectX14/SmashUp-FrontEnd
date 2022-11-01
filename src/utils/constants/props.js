d6395a2f85564a9ccc997952fd6f3350f012355b
Schoolboy1337 <sergeigorobtsev@gmail.com>
2022-11-01 19:55:55 +0400

    feat: добавлены константы пробок

---
+++ src/utils/constants/props.js
@@ -0,0 +1,6 @@
+export const PROP_TYPE_ANY = undefined;
+export const PROP_TYPE_ARRAY = Array;
+export const PROP_TYPE_BOOLEAN = Boolean;
+export const PROP_TYPE_NUMBER = Number;
+export const PROP_TYPE_OBJECT = Object;
+export const PROP_TYPE_STRING = String;
